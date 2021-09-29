ff94cfc9db71a31047465b49ca12d54b800e907d
MisterBolt <bielecki.karol.private@gmail.com>
2021-09-29 14:40:23 +0200

    Add reseting game after refresh page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Constant elements
+const diceElement = document.querySelector(".dice");
+const score0Element = document.getElementById("score--0");
+const score1Element = document.getElementById("score--1");
+const current0Element = document.getElementById("current--0");
+const current1Element = document.getElementById("current--1");
+
+// Starting conditions
+diceElement.classList.add("hidden");
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+current0Element.textContent = 0;
+current1Element.textContent = 0;
