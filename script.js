234db8b782aabe638642f47b9801c0033fcb4924
MisterBolt <bielecki.karol.private@gmail.com>
2021-09-29 15:26:08 +0200

    Add roll button functionality

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 "use strict";
 
-// Constant elements
-const diceElement = document.querySelector(".dice");
+////////////////////---------- SELECTING ELEMENTS ----------\\\\\\\\\\\\\\\\\\\\
+const player0Element = document.querySelector(".player--0");
+const player1Element = document.querySelector(".player--1");
 const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
 const current0Element = document.getElementById("current--0");
 const current1Element = document.getElementById("current--1");
 
-// Starting conditions
+const diceElement = document.querySelector(".dice");
+const rollButton = document.querySelector(".btn--roll");
+
+////////////////////---------- STARTING CONDITIONS ----------\\\\\\\\\\\\\\\\\\\\
+let currentScore = 0;
+let activePlayer = 0;
+
 diceElement.classList.add("hidden");
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 current0Element.textContent = 0;
 current1Element.textContent = 0;
+
+////////////////////---------- BUTTONS FUNCTIONALITY ----------\\\\\\\\\\\\\\\\\\\\
+rollButton.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceElement.src = `images/dice-${dice}.png`;
+  diceElement.classList.remove("hidden");
+
+  if (dice !== 1) {
+    currentScore += dice;
+    displayCurrentScore();
+  } else {
+    switchPlayer();
+  }
+});
+
+////////////////////---------- AUXILIARY FUNCTIONS ----------\\\\\\\\\\\\\\\\\\\\
+function displayCurrentScore() {
+  if (activePlayer === 0) {
+    current0Element.textContent = currentScore;
+  } else {
+    current1Element.textContent = currentScore;
+  }
+}
+
+function switchPlayer() {
+  currentScore = 0;
+  displayCurrentScore();
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle("player--active");
+  player1Element.classList.toggle("player--active");
+}
